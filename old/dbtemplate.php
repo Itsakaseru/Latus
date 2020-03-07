b4c076dcde49b518fbf8739ba08f28c5de0f314a
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-03-07 16:52:20 +0700

    archive

---
+++ old/dbtemplate.php
@@ -0,0 +1,17 @@
+<?php
+    $host = "fdb21.atspace.me";
+    $dbname = "3336533_cocodb";
+    $username = "3336533_cocodb";
+    $password = "5@y@B154B3rD1r1";
+
+    $db = new mysqli($host, $username, $password, $dbname);
+
+	$query = "SELECT * FROM test;";
+	$result = $db->query($query);
+
+    while($row = $result -> fetch_assoc()) {
+		echo $row['testcol'];
+	}
+	mysqli_free_result($result);
+	mysqli_close($db);
+?>
