d82c6c25dad22176314bbfc7aa74e8745acc9a91
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-03-10 12:47:53 +0700

    fix loop only 5 times and not by length

--- php-files/include/randomstr.php
+++ php-files/include/randomstr.php
@@ -2,7 +2,7 @@
     function rndStr($length) {
         $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
         $result = '';
-        for ($i = 0; $i < 5; $i++) $result .= $characters[mt_rand(0, 61)];
+        for ($i = 0; $i < $length; $i++) $result .= $characters[mt_rand(0, 61)];
         return $result;
     }
  ?>
