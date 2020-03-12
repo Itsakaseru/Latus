62e9b3d69973e78a59d113eae8c67121580e86e4
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-03-13 00:43:37 +0700

    change profile html to php version and add basic upload

---
+++ php-files/controller/uploadCover.php
@@ -0,0 +1,85 @@
+<?php
+
+    // for profile pictures, the recommened size is 1024x1024 (user bewarned)
+    // for profile cover, the recommened size is 1500x1000 (check if can) [need testing]
+
+    // get usersId
+    // ------
+
+    // set target dir, file, ONLY ALLOW png, jpg--- 
+    // make sure image file is less than 8mb ----
+    // 
+
+    if(!empty($_FILES["file"])) {
+
+        $email = "getEmailFromDatabase" . "SjyChyalatusIncoporated";
+        $userId = 1;
+
+        $target_dir = "../assets/img/covers/";
+        $temp = explode(".", $_FILES["file"]["name"]);
+
+        // Hash users email+customsalt to be use as picture name
+        $newFileName = hash('sha256', $email) . '.' . end($temp);
+        $target_file = $target_dir . $newFileName;
+
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+        // Check if image file is a actual image or fake image
+        $check = getimagesize($_FILES["file"]["tmp_name"]);
+        $imageWidth = $check[0];
+        $imageHeight = $check[1];
+
+        if($check !== false) {
+            if($imageWidth == 1500 && $imageHeight == 1000) {
+                $uploadOk = 1;
+            } else {
+                $uploadOk = 0;
+                echo "errRes";
+            }
+            
+        } else {
+            echo "errType";
+            $uploadOk = 0;
+        }
+        
+        // Check file size
+        if ($_FILES["file"]["size"] > 8000000) {
+            echo "errLarge";
+            $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
+            echo "errTypeImage";
+            $uploadOk = 0;
+        }
+
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "errFail";
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+
+                include "../include/db_connect.php";
+
+                $query = "UPDATE user SET pic = '" . $target_file . "' WHERE userId = '" . $userId . "';";
+
+                if ($db->query($query) === TRUE) {
+                    echo "success";
+                } else {
+                    echo "Error updating record: " . $conn->error;
+                }
+            } else {
+                echo "errFail";
+            }
+        }
+    }
+    else {
+        echo "Please select image to upload";
+    }
+
+    
+
+?>
---
+++ php-files/profile/index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Latus</title>
+    <link rel="stylesheet" href="../assets/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/profile.css?ver=1.0.0">
+    <script src="../assets/jquery-3.4.1.js"></script>
+    <script src="../assets/bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/a81849e810.js"></script>
+    <script src="../assets/autosize.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<!--
+    PHP find users default theme color
+        -- set as variable
+    echo variable to css style below
+
+    users can choose pre-determined color to avoid conflict with font-color
+-->
+
+<body id="profile">
+    <header data-toggle="modal" data-target="#changeCover"></header>
+    <nav class="navbar navbar-dark navbar-expand-md fixed-top">
+        <a href="/" class="navbar-brand">
+            <img src="../assets/img/web/logo-small.svg" alt="Latus Logo" width="30px;">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse justify-content-stretch" id="navbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Discovery</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <div class="btn-group">
+                        <button id="accBtn" type="button" class="btn d-flex align-items-center justify-content-center" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a>Itsakaseru &nbsp;</a>
+                            <img id="accImg" src="../assets/img/users/itsakaseru.png" alt="itsakaseru" width="30px">
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <button class="dropdown-item" type="button">Profile</button>
+                            <div class="dropdown-divider"></div>
+                            <button class="dropdown-item" type="button">Logout</button>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container pt-5">
+
+        <div class="row">
+            <div id="profileInfo" class="col-3 mr-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div id="profileImage" class="row justify-content-center" data-toggle="modal" data-target="#changePicture">
+                            <img id="profilePicture" class="rounded-circle" src="../assets/img/users/itsakaseru.png" width="160px;" style="border: 5px solid #7E6BC4; border-style: outset;">
+                            <div class="middle">
+                                <div class="text">Change Picture</div>
+                            </div>
+                        </div>
+                        <div id="profileName" class="row justify-content-center mt-3 pl-3 pr-3" style="text-align: center;">
+                            Remueru Itsakaseru
+                        </div>
+                        <hr style="border: 1px solid #B1B1B1">
+                        <div class="profileDetail row justify-content-center">
+                            20 years old
+                        </div>
+                        <div class="profileDetail row justify-content-center">
+                            727 total posts
+                        </div>
+                        <div id="profileControl" class="row justify-content-center mt-4">
+                            <a><i class="fas fa-edit"></i> edit profile</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+
+                <div class="row mb-md-4">
+                    <div class="card col-12 pt-3 pb-3">
+                        <form id="createPost" class="col">
+                            <div class="row">
+                                <textarea id="content" class="inputField form-control" rows="1" name="content" maxlength="256" placeholder="me want post something..."></textarea>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col p-0">
+                                    <button id="imgBtn" name="postContent" type="button" class="btn"><i class="fas fa-file-image"></i></button>  
+                                </div>
+                                <div class="col p-0">
+                                    <button id="postBtn" class="float-right" name="postContent" type="button" class="btn">post</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    
+                </div><!-- End of new post-->
+
+                <div class="row">
+
+                    <div class="containerPost card col-12 pt-3 pb-3">
+                        <div class="row pl-3 d-flex">
+                            <div class="col-9 align-self-start d-flex">
+                                <img class="profilePicture rounded-circle" src="../assets/img/users/itsakaseru.png" width="50px;" style="border: 2px solid #7E6BC4">
+                                <span class="postName ml-3 my-auto">Remueru Itsakaseru</span>
+                            </div>
+                            <div class="col-3 my-auto d-flex">
+                                <span class="ml-auto mr-3">Apr, 14 2020</span>
+                            </div>
+                        </div>
+                        <div class="row pl-3 pt-3">
+                            <div class="col">
+                                KOKO DA YO~
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="commentContainer row pl-3 mb-3">
+                            <div class="col-1 align-self-start d-flex">
+                                <img class="profilePicture rounded-circle" src="../assets/img/users/giovanna.png" width="50px;" style="border: 2px solid #7E6BC4">
+                            </div>
+                            <div class="col-11 d-flex">
+                                <span class="comment my-auto mr-3"><strong>Hans Adhitio</strong> This is a text that will show anything in the comment section of this person original POST who play osu! some counter strike and arknights</span>
+                            </div>
+                        </div>
+
+                        <div class="commentContainer row pl-3 mb-3">
+                            <div class="col-1 align-self-start d-flex">
+                                <img class="profilePicture rounded-circle" src="../assets/img/users/giovanna.png" width="50px;" style="border: 2px solid #7E6BC4">
+                            </div>
+                            <div class="col-11 d-flex">
+                                <span class="comment my-auto mr-3"><strong>Hans Adhitio</strong> This is a text that will show anything in the comment section of this person original POST who play osu! some counter strike and arknights</span>
+                            </div>
+                        </div>
+
+                        <div class="replayContainer">
+                            <form class="col">
+                                <div class="row">
+                                    <div class="col-10">
+                                        <textarea class="inputField form-control" rows="1" name="content" maxlength="256" placeholder="reply something..."></textarea>
+                                    </div>
+                                    <div class="col-2">
+                                        <button class="commentBtn float-right" name="postContent" type="button" class="btn">comment</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div><!-- End of containerPost -->
+
+                </div><!-- End of x-list post -->
+
+            </div>
+        </div><!-- End of contents-->
+    </div>
+
+    <!-- Change Picture Modal -->
+    <div class="modal fade" id="changePicture" tabindex="-1" role="dialog" aria-labelledby="changePicture" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <form method="post" enctype="multipart/form-data">
+                        Select image to upload:
+                        <input id="profilePictureFile" type="file" name="fileToUpload">
+                        <input id="uploadPP" type="button" value="upload profile">
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Change Cover Modal -->
+    <div class="modal fade" id="changeCover" tabindex="-1" role="dialog" aria-labelledby="changeCover" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <form method="post" enctype="multipart/form-data">
+                        Select image to upload:
+                        <input id="coverPictureFile" type="file" name="fileToUpload">
+                        <input id="uploadCP" type="button" value="upload cover">
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+<script>
+    $(document).ready(function() {
+        // to enable solid background for navbar
+        $(window).scroll(function() {
+          if($(this).scrollTop() > 300) { 
+              $('.navbar').addClass('solid');
+          } else {
+              $('.navbar').removeClass('solid');
+          }
+        });
+
+        // enable auto sizing
+        autosize($('textarea'));
+
+        // Change Profile Picture AJAX
+        $('#uploadPP').on('click', function() {
+            var file_data = $("#profilePictureFile").prop("files")[0];   
+            var form_data = new FormData();
+            form_data.append("file", file_data);
+            $.ajax({
+                url: "controller/uploadPicture.php",
+                dataType: 'script',
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: form_data,
+                type: 'post',
+                success: function(data){
+                    if(data == 'success') {
+                        // refresh page
+                    } else {
+                        alert(data);
+                    }
+                },
+                error: function() {
+                    alert("Something went wrong");
+                }
+            });
+        });
+
+        $('#uploadCP').on('click', function() {
+            var file_data = $("#coverPictureFile").prop("files")[0];   
+            var form_data = new FormData();
+            form_data.append("file", file_data);
+            $.ajax({
+                url: "controller/uploadCover.php",
+                dataType: 'script',
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: form_data,                         
+                type: 'post',
+                success: function(data){
+                    if(data == 'success') {
+                        // refresh page
+                    }
+                },
+                error: function() {
+                    alert("Something went wrong");
+                }
+            });
+        });
+    });
+</script>
+
+</html>
