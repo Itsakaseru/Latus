804a14204a574c86bdd757f7862f0ef727bc54c9
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-03-21 13:53:13 +0700

    add ability to add picture in post and modify mysql to use prepare statement
    add necessary file to load data from database

--- php-files/controller/addPost.php
+++ php-files/controller/addPost.php
@@ -6,13 +6,17 @@
         //--- if yes then set database image to link image location
         //--- if not then set database image to null
 
+        include "../include/randomstr.php";
         include "../include/db_connect.php";
 
         // Get userID from token
         $token = $_COOKIE["latus-token"];
 
-        $query = "SELECT userId FROM user WHERE token = '" . $token . "';";
-        $result = $db->query($query);
+        $query = $db->prepare("SELECT userId FROM user WHERE token = ?");
+        $query->bind_param("s", $token);
+
+        $query->execute();
+        $result = $query->get_result();
 
         if(mysqli_num_rows($result) == 1) {
             $data = $result->fetch_assoc();
@@ -22,20 +26,90 @@
             exit();
         }
 
-        $contents = $_POST["content"];
-        $img = "null";
+        // Get Latest postId
+        $query = "SELECT postId FROM timeline ORDER BY postId DESC LIMIT 1";
+        $result = $db->query($query);
 
-        $query = "INSERT INTO timeline (userId, contents, pic)
-                  VALUES ('" . $userId . "', '" . $contents . "', '" . $img . "');";
+        if(mysqli_num_rows($result) == 0) {
+            // If there is none
+            $postId = 1;
+        } else {
+            $data = $result->fetch_assoc();
+            $postId = (int) $data["postId"];
+            $postId++;
+        }
+
+        // If user wan't to upload image file aswell
+        if(isset($_FILES["file"])){
+
+            // Set upload to yes
+            $uploadOk = 1;
+
+            // Generate hash use for image name
+            $hash = $postId . $userId . time() . rndStr(5);
+            $hash = hash('sha256', $hash);
+
+            // Check if image file is a actual image or not
+            $check = getimagesize($_FILES["file"]["tmp_name"]);
+            if($check !== false) {
+                $uploadOk = 1;
+            } else {
+                echo "errType";
+                exit();
+            }
+
+            $target_dir = "../assets/img/posts/" . $userId . "/";
+            $temp = explode(".", $_FILES["file"]["name"]);
+            $newFileName = $hash . '.' . end($temp);
+            $img = $target_dir . $newFileName;
+        }
+        else {
+            // No Upload Neccessary
+            $uploadOk = 0;
+            $img = "null";
+        }
+
+        if($uploadOk == 0) {
+            insertData($userId, $img);
+            exit();
+        }
+        else if($uploadOk == 1) {
+            if(!is_dir($target_dir)) {
+                mkdir($target_dir);
+            }
+
+            if (move_uploaded_file($_FILES["file"]["tmp_name"], $img)) {
+                insertData($userId, $img);
+                exit();
+            } else {
+                echo "errFail";
+            }
+        }
         
-        if($db->query($query) === TRUE) {
+    }
+    else {
+        echo "nah";
+    }
+
+    function insertData($userId, $img) {
+
+        // Open database connection
+        include "../include/db_connect.php";
+
+        // Get content from POST Data
+        $contents = $_POST["content"];
+        
+        $query = $db->prepare("INSERT INTO timeline (userId, contents, pic)
+                                VALUES (?, ?, ?)");
+        $query->bind_param("iss", $userId, $contents, $img);
+        
+        if($query->execute()) {
             echo "success";
         } else {
             echo $db->error;
         }
-    }
-    else {
-        echo "nah";
+
+        $db->close();
     }
 
 ?>
