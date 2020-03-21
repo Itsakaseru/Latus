c86ab09087d592217679c08947d47845661cd3b5
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-03-21 13:49:30 +0700

    modify add comment to use mysql prepare statement

--- php-files/controller/addComment.php
+++ php-files/controller/addComment.php
@@ -5,9 +5,15 @@
     
     include "../include/db_connect.php";
 
+    // Get User Token
     $token = $_COOKIE["latus-token"];
-    $query = "SELECT userId FROM user WHERE token = '" . $token . "';";
-    $result = $db->query($query);
+
+    // Get User ID from token
+    $query = $db->prepare("SELECT userId FROM user WHERE token = ?");
+    $query->bind_param("s", $token);
+    $query->execute();
+
+    $result = $query->get_result();
 
     if(mysqli_num_rows($result) == 1) {
         $data = $result->fetch_assoc();
@@ -20,10 +26,11 @@
     $postId = $_POST["postId"];
     $comment = $_POST["comment"];
 
-    $query = "INSERT INTO comment (userId, postId, contents)
-              VALUES ('" . $userId . "', '" . $postId . "', '" . $comment . "');";
+    $query = $db->prepare("INSERT INTO comment (userId, postId, contents)
+                           VALUES (?, ?, ?)");
+    $query->bind_param("iis", $userId, $postId, $comment);
 
-    if($db->query($query) === TRUE) {
+    if($query->execute()) {
         echo "success";
     }
     else {
