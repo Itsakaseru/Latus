91adf20c599368e9499bcb858a012a24d334b93b
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-03-23 02:49:47 +0700

    add 404 page not found

---
+++ php-files/404/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latus - 404</title>
+    <link rel="stylesheet" href="../assets/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="404.css?ver=1.0.0">
+    <script src="../assets/jquery-3.4.1.js"></script>
+    <script src="../assets/bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="for-container" style="overflow: hidden;">
+    <div class="left">
+        <h1 class="text-center">Oops! Something went wrong!</h1>
+        <h4 style="text-align: center; text-decoration: none !important;">404 Not Found~</h4>
+        <div class="btnCenter">
+            <a class="btn" href="../">Return to Home</a>
+        </div>
+    </div>
+    <div class="right">
+        <img src="404.jpg" alt=":3"/>
+    </div>
+</div>
+</body>
+</html>
