1eacdc2ab01d837748bccb246a6ef59d8da7584b
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-03-24 15:29:07 +0700

    fix typo text in profile when changing cover picture

--- php-files/profile/index.php
+++ php-files/profile/index.php
@@ -379,7 +379,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Change Profile Picture</h5>
+                    <h5 class="modal-title">Change Cover Picture</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
